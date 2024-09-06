aded8c6175d6d0ce81e084e62a7da1ed2165392d
NingKe <forwork.cm@gmail.com>
2024-09-06 15:17:16 +0800

    add AI check

--- Frontend/app/create/page.tsx
+++ Frontend/app/create/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { TagType } from "@/app/types";
 import { useState, useEffect, useReducer } from "react";
+import { useFormState } from "react-dom";
+import Image from "next/image";
+import { createNFT } from "./actions";
+import SubmitButton from "../components/SubmitButton";
+import { Dialog, DialogPanel } from "@headlessui/react";
 
 type Props = {
   followTopics: TagType[];
@@ -77,49 +82,61 @@ function reducer(state: InitialState, action: Action) {
     }
   }
 }
+type InitialFormState = {
+  message: string;
+  type: "success" | "fail";
+  data?: number;
+};
+const initialFormState: InitialFormState = { message: "", type: "fail" };
 
 export default function Page() {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [currentGrade, setCurrentGrade] = useState(0);
+
+  const [formState, formAction] = useFormState(createNFT, initialFormState);
+
+  const [isOpen, setIsOpen] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  const onClose = () => {
+    if (loading) return;
+    if (formState.type === "success") setIsOpen(false);
+  };
   return (
     <>
-      <form className="mx-auto">
+      <form className="mx-auto" action={formAction}>
         <div className="space-y-12">
           <div>
-            <h2 className="text-base font-semibold leading-7 text-center text-gray-900">
+            <h2 className="text-base font-semibold leading-7 text-center text-[#666]">
               发布创意
             </h2>
 
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="col-span-full">
-                <label className="block text-xs leading-6 text-gray-900">
-                  标题
-                </label>
+                <label className="block leading-6 text-[#666]">标题</label>
                 <div className="mt-2">
                   <input
                     id="title"
                     name="title"
                     required
                     placeholder="请输入创意名称，仅支持中文、英文、数字字符"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-xs sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 text-[#666] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-[#ccc] focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="col-span-full">
-                <label className="block text-xs leading-6 text-gray-900">
-                  摘要
-                </label>
+                <label className="block leading-6 text-[#666]">摘要</label>
                 <div className="mt-2">
                   <input
                     id="title"
                     name="title"
                     placeholder="请输入摘要内容，明确描述清楚该创意亮点及解决的问题"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-xs sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 text-[#666] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-[#ccc] focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="col-span-full">
-                <label className="block text-xs leading-6 text-gray-900">
+                <label className="block leading-6 text-[#666]">
                   主题关键词
                 </label>
                 <div className="mt-2">
@@ -127,20 +144,20 @@ export default function Page() {
                     id="title"
                     name="title"
                     placeholder="请输入1-5个关键词，请用英文;隔开"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-xs sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 text-[#666] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-[#ccc] focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="col-span-full">
                 <div className="flex justify-between">
-                  <label className="block text-xs leading-6 text-gray-900">
+                  <label className="block leading-6 text-[#666]">
                     创意详设
                   </label>
-                  <p className="text-gray-500">
+                  <p className="text-[#666]">
                     <input
                       id="isFree"
                       type="checkbox"
-                      className="mr-2 rounded-sm border-gray-300 focus:shadow-none focus:ring-offset-0 focus:ring-0"
+                      className="mr-2 rounded-sm border-[#666] focus:shadow-none focus:ring-offset-0 focus:ring-0"
                     />
                     免费公开
                   </p>
@@ -150,7 +167,8 @@ export default function Page() {
                     id="about"
                     name="about"
                     rows={5}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-xs sm:leading-6"
+                    placeholder="请输入创意的详细内容、使用指南和预期效果，字数不限，可作为收益内容。"
+                    className="block w-full rounded-md border-0 py-1.5 text-[#666] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-[#ccc] focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
                   ></textarea>
                 </div>
               </div>
@@ -158,15 +176,13 @@ export default function Page() {
           </div>
 
           <div className="sm:col-span-3">
-            <label className="block text-xs leading-6 text-gray-900">
-              学科分类
-            </label>
+            <label className="block leading-6 text-[#666]">学科分类</label>
             <div className="mt-2 flex gap-2 items-start">
               <select
                 id="grade"
                 name="grade"
                 autoComplete="grade-name"
-                className="block w-20 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-xs sm:leading-6"
+                className="block w-20 rounded-md border-0 py-1.5 text-[#666] shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-primary sm:max-w-xs sm:text-sm sm:leading-6"
               >
                 {grades.map((item) => (
                   <option>{item.name}</option>
@@ -177,7 +193,7 @@ export default function Page() {
                 {state.subjects.map((item) => (
                   <div
                     key={item.key}
-                    className="flex items-center gap-2 text-[0.6rem] px-2 py-0.5 border rounded-full border-blue-400 bg-blue-50 text-blue-400"
+                    className="flex items-center gap-2 text-sm px-4 py-0.5 border rounded-full border-primary bg-primary-light text-primary"
                     onClick={() =>
                       item.key !== 0 &&
                       dispatch({ type: "remove", payload: item })
@@ -196,7 +212,7 @@ export default function Page() {
                     >
                       <path
                         d="M557.12 512l313.6-313.28a32.128 32.128 0 1 0-45.44-45.44L512 466.88l-313.28-313.6a32.128 32.128 0 0 0-45.44 45.44L466.88 512l-313.6 313.28a32 32 0 0 0 0 45.44 32 32 0 0 0 45.44 0L512 557.12l313.28 313.6a32 32 0 0 0 45.44 0 32 32 0 0 0 0-45.44L557.12 512z"
-                        fill="#60a5fa"
+                        fill="#1474FC"
                         p-id="1693"
                       ></path>
                     </svg>
@@ -204,26 +220,26 @@ export default function Page() {
                 ))}
                 <input
                   placeholder="支持新增专业分类，多个专业分类用英文;隔开，回车键结束"
-                  className="text-[0.7rem] w-80 py-0.5 placeholder:text-slate-400 border-none outline-none shadow-none focus:shadow-none focus:ring-offset-0 focus:ring-0"
+                  className="text-sm w-[400px] py-0.5 placeholder:text-[#CCC] border-none outline-none shadow-none focus:shadow-none focus:ring-offset-0 focus:ring-0"
                 />
               </div>
             </div>
 
             {/* subject start */}
-            <div className="flex flex-wrap gap-2 mt-2">
+            <div className="flex flex-wrap gap-2 mt-4">
               {subjects.map((item) => (
                 <>
                   {state.subjectKeys.includes(item.key) ? (
                     <div
                       key={item.key}
-                      className="py-1 px-2 rounded-full text-[0.7rem] font-light bg-blue-400 text-white"
+                      className="py-1 px-2 rounded-full text-sm font-light bg-primary text-white"
                     >
                       {item.name}
                     </div>
                   ) : (
                     <div
                       key={item.key}
-                      className="py-1 px-2 rounded-full text-[0.7rem] font-light border border-blue-400 text-blue-400 cursor-pointer"
+                      className="py-1 px-2 rounded-full text-sm font-light border border-primary text-primary cursor-pointer"
                       onClick={() => dispatch({ type: "add", payload: item })}
                     >
                       <svg
@@ -238,7 +254,7 @@ export default function Page() {
                       >
                         <path
                           d="M892.16 480H544V131.84a32 32 0 0 0-64 0V480H131.84a32 32 0 0 0 0 64H480v348.16a32 32 0 1 0 64 0V544h348.16a32 32 0 1 0 0-64z"
-                          fill="#60a5fa"
+                          fill="#1474FC"
                           p-id="1842"
                         ></path>
                       </svg>
@@ -251,28 +267,75 @@ export default function Page() {
             {/* subject end */}
           </div>
           <div className="sm:col-span-3">
-            <label className="block text-xs leading-6 text-gray-900">
-              售价
-            </label>
+            <label className="block leading-6 text-[#666]">售价</label>
             <div className="mt-2">
               <input
                 id="title"
                 name="title"
                 placeholder="0.00ETH"
-                className="block w-full sm:max-w-xs rounded-md border-0 py-1.5 text-gray-900 text-right shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-xs sm:leading-6"
+                className="block w-full sm:max-w-xs rounded-md border-0 py-1.5 text-[#666] text-right shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-[#ccc] focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
               />
             </div>
           </div>
         </div>
 
-        <div className="mt-10 flex justify-between">
-          <button
-            type="submit"
-            disabled={true}
-            className="rounded-md bg-indigo-600 px-10 py-2 mx-auto text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-indigo-400 disabled:cursor-not-allowed"
-          >
-            发布
-          </button>
+        <div className="w-[174px] mx-auto mt-10">
+          <SubmitButton>发布</SubmitButton>
+
+          <Dialog open={isOpen} onClose={onClose} className="relative z-5">
+            <div className="fixed inset-0 flex w-screen h-screen items-center justify-center p-4 bg-black/40">
+              <DialogPanel className="w-[496px] py-12 flex flex-col justify-between items-center gap-6 space-y-4 bg-white rounded-lg overflow-hidden">
+                {loading && (
+                  <>
+                    <p className="text-[#333] text-lg">查重校对中，请等待…</p>
+                    <div className="flex w-14 h-24 relative space-x-2">
+                      <div className="w-2.5 h-2.5 absolute left-2 bottom-0 bg-blue-500 rounded-full animate-[loading_1s_ease-in-out_infinite]"></div>
+
+                      <div className="w-2.5 h-2.5 absolute left-4 bottom-0 bg-blue-500 rounded-full animate-[loading_1s_ease-in-out_infinite] [animation-delay:0.2s]"></div>
+
+                      <div className="w-2.5 h-2.5 absolute left-8 bottom-0 bg-blue-500 rounded-full animate-[loading_1s_ease-in-out_infinite] [animation-delay:0.4s]"></div>
+
+                      <div className="w-2.5 h-2.5 absolute left-12 bottom-0 bg-blue-500 rounded-full animate-[loading_1s_ease-in-out_infinite] [animation-delay:0.6s]"></div>
+                    </div>
+                  </>
+                )}
+                {!loading && formState.type === "success" && (
+                  <>
+                    <p className="text-[#333] text-lg">查重校对已通过</p>
+                    <Image
+                      src={"/images/slice/check_ok.png"}
+                      width={48}
+                      height={48}
+                      alt="check ok"
+                    />
+                  </>
+                )}
+                {!loading && formState.type === "fail" && (
+                  <>
+                    <p className="text-[#333] text-lg">
+                      查重校对结果：
+                      <span className="text-[#F06A6A]">
+                        {formState.data ?? 0}%
+                      </span>
+                      ，请返回修改！
+                    </p>
+                    <Image
+                      src={"/images/slice/check_fail.png"}
+                      width={48}
+                      height={48}
+                      alt="check fail"
+                    />
+                    <div
+                      onClick={() => setIsOpen(false)}
+                      className="rounded-md bg-primary px-6 py-2 mx-auto font-semibold text-white shadow-sm cursor-pointer hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-light/50 "
+                    >
+                      返回修改
+                    </div>
+                  </>
+                )}
+              </DialogPanel>
+            </div>
+          </Dialog>
         </div>
       </form>
     </>
--- Frontend/tailwind.config.ts
+++ Frontend/tailwind.config.ts
@@ -26,6 +26,20 @@ const config: Config = {
           font_C: "#CCC",
         },
       },
+
+      keyframes: {
+        // 自定义动画: 圆角柱从长到短，透明度从小到大
+        loading: {
+          "0%, 100%": { height: "10px", opacity: "0.1" },
+          "25%": { height: "10px", opacity: "0.2" },
+          "50%": { height: "12px", opacity: "0.5" },
+          "75%": { height: "20px", opacity: "1" },
+        },
+      },
+      animation: {
+        // 动画持续时间和无限循环
+        loading: "loading 3s ease-in-out infinite",
+      },
     },
   },
   plugins: [require("@tailwindcss/forms")],
