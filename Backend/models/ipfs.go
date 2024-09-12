48485c90d1d3ee5e980fe89a6bd8ee1a9a812351
xiaoyuan <lony0116@163.com>
2024-09-12 16:34:39 +0800

    ipfs修复

--- Backend/models/ipfs.go
+++ Backend/models/ipfs.go
@@ -1,7 +1,40 @@
 package models
 
+import "time"
+
 type Metadata struct {
 	Title   string `json:"title"`
 	Summary string `json:"summary"`
 	Content string `json:"content"`
+}
+
+type IpfsResponseData struct {
+	IpfsHash    string    `json:"IpfsHash"`
+	PinSize     int       `json:"PinSize"`
+	Timestamp   time.Time `json:"Timestamp"`
+	IsDuplicate bool      `json:"isDuplicate"`
+}
+
+type Region struct {
+	RegionID                string `json:"regionId"`
+	CurrentReplicationCount int    `json:"currentReplicationCount"`
+	DesiredReplicationCount int    `json:"desiredReplicationCount"`
+}
+
+type Row struct {
+	ID            string    `json:"id"`
+	IPFSPinHash   string    `json:"ipfs_pin_hash"`
+	Size          int       `json:"size"`
+	UserID        string    `json:"user_id"`
+	DatePinned    time.Time `json:"date_pinned"`
+	DateUnpinned  time.Time `json:"date_unpinned"`
+	Metadata      Metadata  `json:"metadata"`
+	Regions       []Region  `json:"regions"`
+	MimeType      string    `json:"mime_type"`
+	NumberOfFiles int       `json:"number_of_files"`
+}
+
+type IpfsData struct {
+	Count int   `json:"count"`
+	Rows  []Row `json:"rows"`
 }
--- Backend/service/ipfs_service.go
+++ Backend/service/ipfs_service.go
@@ -1,12 +1,8 @@
 package service
 
 import (
-	"bytes"
-	"encoding/json"
 	"nftPlantform/api"
 	"nftPlantform/models"
-
-	"github.com/sirupsen/logrus"
 )
 
 type IPFSService struct {
@@ -19,32 +15,12 @@ func NewIPFSService(client api.IPFSRepository) *IPFSService {
 	}
 }
 
-func (s *IPFSService) GetData(hash string) (*models.Metadata, error) {
-	var nftMetaData models.Metadata
-	data, err := s.client.GetData(hash)
-	if err != nil {
-		logrus.Errorf("Failed to get data from IPFS: %v", err)
-		return nil, err
-	}
-	err = json.Unmarshal(data, &nftMetaData)
-	if err != nil {
-		logrus.Errorf("Failed to unmarshal NFT metadata: %v", err)
-		return nil, err
-	}
-	return &nftMetaData, nil
+func (s *IPFSService) GetData(ipfsHash string) (*models.IpfsData, error) {
+	_, err := s.client.GetData(ipfsHash)
+	return nil, err
 }
 
-func (s *IPFSService) UploadData(nftMetaData models.Metadata) (string, error) {
-	jsonData, err := json.Marshal(nftMetaData)
-	if err != nil {
-		logrus.Errorf("Failed to marshal NFT metadata: %v", err)
-		return "", err
-	}
-	hash, err := s.client.UploadData(bytes.NewReader(jsonData))
-	if err != nil {
-		logrus.Errorf("Failed to upload data to IPFS: %v", err)
-		return "", err
-	}
-	logrus.Infof("Successfully uploaded NFT metadata to IPFS with hash: %s", hash)
-	return hash, nil
+func (s *IPFSService) UploadData(createRequest interface{}) (*models.IpfsResponseData, error) {
+	ipfsData, err := s.client.UploadData(createRequest)
+	return ipfsData, err
 }
